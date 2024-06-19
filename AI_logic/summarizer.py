d57497a98912f9d2a4411d9928699dbc2cda88b4
andrew <andry925@ukr.net>
2024-06-19 22:07:52 +0300

    refactor: refactor summarizer

--- AI_logic/summarizer.py
+++ AI_logic/summarizer.py
@@ -4,38 +4,41 @@ from langchain.chains.summarize import load_summarize_chain
 from langchain_community.llms import HuggingFaceHub
 
 
-def divide_text_on_chunks(text):
-    text_splitter = CharacterTextSplitter(chunk_size=10, chunk_overlap=5)
-    texts = text_splitter.split_text(text)
-    return texts
+class SimpleSummarizer:
 
+    def __init__(self):
+        self.chunk_size = 10
+        self.chunk_overlap = 5
+        self.max_length = 512
+        self.temperature = 0.5
+        self.repo_id = 'facebook/bart-large-cnn'
 
-def convert_chunks_to_document(text_input):
-    splited_text = divide_text_on_chunks(text=text_input)
-    docs = [Document(page_content=text) for text in splited_text]
-    return docs
+    def divide_text_on_chunks(self, text):
+        text_splitter = CharacterTextSplitter(
+            chunk_size=self.chunk_size,
+            chunk_overlap=self.chunk_overlap)
+        texts = text_splitter.split_text(text)
+        return texts
 
+    def convert_chunks_to_document(self, text_input):
+        splited_text = self.divide_text_on_chunks(text=text_input)
+        docs = [Document(page_content=text) for text in splited_text]
+        return docs
 
-def load_llm():
-    llm = HuggingFaceHub(repo_id='facebook/bart-large-cnn',
-                         model_kwargs={'max_length': 512, 'temperature': 0.5}
-                         )
-    return llm
+    def load_llm(self):
+        llm = HuggingFaceHub(
+            repo_id=self.repo_id,
+            model_kwargs={
+                'max_length': self.max_length,
+                'temperature': self.temperature})
+        return llm
 
+    def chain_document(self, docs):
+        llm = self.load_llm()
+        chain = load_summarize_chain(llm, chain_type='map_reduce')
+        return chain.run(docs)
 
-def chain_document(docs):
-    llm = load_llm()
-    chain = load_summarize_chain(llm, chain_type='map_reduce')
-    return chain.run(docs)
-
-
-def run_summarizer(text_input):
-    docs = convert_chunks_to_document(text_input=text_input)
-    response = chain_document(docs)
-    yield response.strip()
-
-
-if __name__ == "__main__":
-    input_text = input("Enter a sentence: ")
-    for sentence in run_summarizer(input_text):
-        print(sentence)
+    def run_summarizer(self, text_input):
+        docs = self.convert_chunks_to_document(text_input=text_input)
+        response = self.chain_document(docs)
+        yield response.strip()
